e96a866408c874c269f6dc7948d57b9f541f3459
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-30 21:58:39 -0300

    create a user

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,48 @@
 import fastify from 'fastify';
+import { randomUUID } from 'node:crypto'
 
 const app = fastify();
 
+interface UserProps {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const listOfUsers: UserProps[] = [];
+
 app.get("/", () => {
   return { hello: "world" };
+});
+
+app.get("/users", () => {
+  return listOfUsers;
+});
+
+app.post("/users", (req , reply) => {
+  const { name, email } = req.body as UserProps;
+
+  const userAlreadyExists = listOfUsers.some((user) => user.email === email);
+
+  if (userAlreadyExists) {
+    return reply.status(400).send(
+      {
+        error: "User already exists!",
+      }
+    );
+  }
+
+  if (name && email) {
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+    };
+
+    listOfUsers.push(user);
+  }
+
+  return reply.status(201).send();
 });
 
 app.listen({
